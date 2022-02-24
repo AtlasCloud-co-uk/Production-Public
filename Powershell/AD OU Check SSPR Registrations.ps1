66049c58ba828b51299b7eec3b56e0fa110577ab
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2022-02-24 11:57:03 +0000

    Update AD OU Check SSPR Registrations.ps1
    Updated comments and remove spare lines

--- Powershell/AD OU Check SSPR Registrations.ps1
+++ Powershell/AD OU Check SSPR Registrations.ps1
@@ -7,6 +7,7 @@ $OU = $OUList | Out-GridView -Title "Select OU and Click OK" -OutputMode Single
 # Gets the AD Users as per the OU selected, Added the LastLogOnDate properties, display the Name. Enablesand LastLogOnDate
 $ADUsers = Get-ADUser -SearchBase $OU.DistinguishedName -Filter * -Properties SSPRQ,LastLogOnDate | Select-Object -Property Name,Enabled,LastLogOnDate,SSPRQ
 
+# Get the list of user confirm SSPRQ and display Output
 $Output = foreach ($ADuser in $ADUsers){
 if ($ADUser.ssprq -eq $Null)
 {
@@ -17,8 +18,6 @@ else
    Write-Host $ADUser.name $ADUser.Enabled "is registered for SSPR." -ForegroundColor Green
 }
 }
-
-
 
 # Clears the Variables, this stops any issue with the variables bring back any previous held information
 Remove-Variable * -ErrorAction SilentlyContinue
