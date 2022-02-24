bf46962b68033764b655b5e42b8b9422f3f74675
DarrenEdgson <darren.edgson@atlascloud.co.uk>
2022-02-24 11:55:49 +0000

    Update AD OU Check SSPR Registrations.ps1

--- Powershell/AD OU Check SSPR Registrations.ps1
+++ Powershell/AD OU Check SSPR Registrations.ps1
@@ -1,8 +1,24 @@
-#Get-AdUser -Properties ssprq -filter {ssprq -like '*'} | FT Name,SamAccountName
+# Gets a list of OU
+$OUList = Get-ADOrganizationalUnit -Filter * -Properties Name,DistinguishedName | Sort-Object | Select-Object -Property Name,DistinguishedName
 
-$ADUsers = Get-AdUser -Properties ssprq -filter {ssprq -like '*'}
+# Create a Gridview list of Groups, to uses as a selection
+$OU = $OUList | Out-GridView -Title "Select OU and Click OK" -OutputMode Single
 
-foreach ($ADUser in $ADUsers)
-    {
-    Write-Host $ADUser.Name
-    }
+# Gets the AD Users as per the OU selected, Added the LastLogOnDate properties, display the Name. Enablesand LastLogOnDate
+$ADUsers = Get-ADUser -SearchBase $OU.DistinguishedName -Filter * -Properties SSPRQ,LastLogOnDate | Select-Object -Property Name,Enabled,LastLogOnDate,SSPRQ
+
+$Output = foreach ($ADuser in $ADUsers){
+if ($ADUser.ssprq -eq $Null)
+{
+   Write-Host $ADUser.name "is NOT registered for SSPR." -ForegroundColor Red
+}
+else
+{
+   Write-Host $ADUser.name $ADUser.Enabled "is registered for SSPR." -ForegroundColor Green
+}
+}
+
+
+
+# Clears the Variables, this stops any issue with the variables bring back any previous held information
+Remove-Variable * -ErrorAction SilentlyContinue
