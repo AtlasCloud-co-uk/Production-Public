33a746fddf2fe5c40641b33e23f40665599a9e26
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2022-02-25 07:13:03 +0000

    Update VMW List Snapshots.ps1
    Corrected spelling

--- Powershell/VMW List Snapshots.ps1
+++ Powershell/VMW List Snapshots.ps1
@@ -7,7 +7,7 @@ $VSphereServerImport = Import-Csv -Path "C:\Powershell Scripts\CSV\VMServers.csv
 # Import Modue vmware viautomation core 
 Import-Module vmware.vimautomation.core
 
-# Connect to teh VMWare server and pulls back the snapsots into a grid-view
+# Connect to the VMWare server and pulls back the snapsots into a grid-view
 Connect-VIServer $VSphereServerImport.Server
 $SnapShotList = Get-VM | Get-SnapShot | Select Created, VM, Name, Description, SizeGB
 $SnapShotCount = $SnapShotList | Measure
