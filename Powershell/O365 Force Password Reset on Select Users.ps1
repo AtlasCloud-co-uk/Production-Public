4ee34d8a532014f19c614f2ab40c980f43474fb4
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2022-09-13 09:06:33 +0100

    New

---
+++ Powershell/O365 Force Password Reset on Select Users.ps1
@@ -0,0 +1,105 @@
+# Connect to Office 365
+Connect-MsolService
+
+# Save CSV Location
+Write-Host "Export Path and CSV file name " -NoNewline
+Write-Host "(Example: c:\Temp\ADUserlist.csv): " -ForegroundColor Green -NoNewline
+$CSVExportPath = Read-Host
+
+# Export Enabled Only User/s Account/s to csv
+Get-MsolUser | Where-Object BlockCredential -match "False" | Sort-Object -Property DisplayName | Select-Object DisplayName,UserPrincipalName,BlockCredential | Export-Csv $CSVExportPath
+
+# Location for import CSV file
+Write-Host "Remove any Admin or Service accounts from the Exported CSV before proceeding" -ForegroundColor Red
+Write-Host "Import Path and CSV File or " -NoNewline
+Write-Host "Press Enter Yes to use previous Path: " -ForegroundColor Green -NoNewline
+$CSVImportPath = Read-Host
+
+# Message Filter Default Input if nothing has been entered
+If ([string]::IsNullOrWhiteSpace($CSVImportPath))
+    {   
+    Write-Host "Existing" -ForegroundColor Green
+    $RevokeSession = Exit
+    }
+else { if ($CSVImportPath -eq "Yes") {
+    $CSVImportPath = $CSVExportPath
+        }
+    }   
+
+# Import CSV file to variable
+$ImportCSV = Import-Csv $CSVImportPath
+
+# Check Import List
+$ImportCSV.UserPrincipalName
+
+# Change ForcePassword on next log based in the Imported CSV file
+Write-Host "Type Yes to Contiune Import? " -NoNewline
+Write-Host "(No is default): " -ForegroundColor Green -NoNewline
+$ForcePassword = Read-Host
+
+# Message Filter Default Input if nothing has been entered
+If ([string]::IsNullOrWhiteSpace($ForcePassword))
+{
+    Write-Host "Existing" -ForegroundColor Green
+    $ForcePassword= Exit
+}
+elseif ($ForcePassword -eq "no") {
+    Write-Host "Existing" -ForegroundColor Green
+    Exit
+  }
+elseif ($ForcePassword -eq "N") {
+    Write-Host "Existing" -ForegroundColor Green
+    Exit
+    }
+elseif ($ForcePassword -eq "Y") {
+    Connect-AzureAD 
+        foreach ($ImportCSV1 in $ImportCSV) {
+        Set-MsolUserPassword -UserPrincipalName $ImportCSV1.UserPrincipalName -ForceChangePasswordOnly $true -ForceChangePassword $true
+    }
+        }
+       
+else {
+    if ($ForcePassword -eq "Yes") {
+        foreach ($ImportCSV1 in $ImportCSV) {
+        Set-MsolUserPassword -UserPrincipalName $ImportCSV1.UserPrincipalName -ForceChangePasswordOnly $true -ForceChangePassword $true
+        }
+  } 
+}
+
+# Revoke User Sessions
+Write-Host "Do you want to Revoke User/s Account? " -NoNewline
+Write-Host "(No is default): " -ForegroundColor Green -NoNewline
+$RevokeSession = Read-Host
+
+# Message Filter Default Input if nothing has been entered
+If ([string]::IsNullOrWhiteSpace($RevokeSession))
+{
+    Write-Host "Existing" -ForegroundColor Green
+    $RevokeSession = Exit
+}
+elseif ($RevokeSession -eq "no") {
+    Write-Host "Existing" -ForegroundColor Green
+    $RevokeSession = Exit
+  }
+elseif ($RevokeSession -eq "N") {
+    Write-Host "Existing" -ForegroundColor Green
+    $RevokeSession = Exit
+    }
+elseif ($RevokeSession -eq "Y") {
+        Connect-AzureAD 
+        foreach ($ImportCSV1 in $ImportCSV) {
+            Get-AzureADUser -SearchString $ImportCSV1.UserPrincipalName | Revoke-AzureADUserAllRefreshToken
+            }
+        }
+       
+else {
+    if ($RevokeSession -eq "Yes") {
+        Connect-AzureAD 
+        foreach ($ImportCSV1 in $ImportCSV) {
+            Get-AzureADUser -SearchString $ImportCSV1.UserPrincipalName | Revoke-AzureADUserAllRefreshToken 
+            }
+        } 
+}
+
+# Clears the Variables, this stops any issue with the variables bring back any previous held information
+Remove-Variable * -ErrorAction SilentlyContinue
