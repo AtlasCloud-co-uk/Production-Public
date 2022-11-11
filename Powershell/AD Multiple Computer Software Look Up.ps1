37c3de0d2462f5f35463152e4b4d7b47d7fb202a
Darren Edgson <darren.edgson@atlascloud.co.uk>
2022-11-11 10:33:58 +0000

    Update AD Multiple Computer Software Look Up.ps1
    Add in 64bit

--- Powershell/AD Multiple Computer Software Look Up.ps1
+++ Powershell/AD Multiple Computer Software Look Up.ps1
@@ -1,3 +1,24 @@
+#############################################################################
+#If Powershell is running the 32-bit version on a 64-bit machine, we 
+#need to force powershell to run in 64-bit mode .
+#############################################################################
+if ($env:PROCESSOR_ARCHITEW6432 -eq "AMD64") {
+    write-warning "Opening 64-bit powershell....."
+    if ($myInvocation.Line) {
+        &"$env:WINDIR\sysnative\windowspowershell\v1.0\powershell.exe" -NonInteractive -NoProfile $myInvocation.Line
+    }else{
+        &"$env:WINDIR\sysnative\windowspowershell\v1.0\powershell.exe" -NonInteractive -NoProfile -file "$($myInvocation.InvocationName)" $args
+    }
+exit $lastexitcode
+}
+
+
+write-host "Starting Main script body"
+
+#############################################################################
+#End
+#############################################################################  
+
 # HTML Code
 
 $header = "
