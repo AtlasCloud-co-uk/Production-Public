081632ce0462663102d5331d8c8a9b1bac82d0e3
Darren Edgson <darren.edgson@atlascloud.co.uk>
2022-11-18 13:33:16 +0000

    New

---
+++ Powershell/AD Computer Log Off Users.ps1
@@ -0,0 +1,132 @@
+﻿# Gets a list of Computers
+$ComputerList = Get-ADComputer -Filter * -Properties Name,DistinguishedName | Sort-Object | Select-Object -Property Name,DistinguishedName
+
+# Select Computer from Gridview
+$ComputerSelect = $ComputerList | Out-GridView -Title "Select Computer Name and Click OK" -OutputMode Multiple
+Write-Host "Computer: " $ComputerSelect.Name
+
+# Function to get users
+Function Get-LoggedInUser {
+ 
+    [CmdletBinding()]
+        param(
+            [Parameter(
+                Mandatory = $false,
+                ValueFromPipeline = $true,
+                ValueFromPipelineByPropertyName = $true,
+                Position=0
+            )]
+            [string[]] $ComputerName = $ComputerSelect.Name,
+ 
+ 
+            [Parameter(
+                Mandatory = $false
+            )]
+            [Alias("SamAccountName")]
+            [string]   $UserName
+        )
+ 
+    BEGIN {}
+ 
+    PROCESS {
+        foreach ($Computer in $ComputerName) {
+            try {
+                $Computer = $Computer.ToUpper()
+                $SessionList = quser /Server:$Computer 2>$null
+                if ($SessionList) {
+                    $Global:UserInfo = foreach ($Session in ($SessionList | select -Skip 1)) {
+                        $Session = $Session.ToString().trim() -replace '\s+', ' ' -replace '>', ''
+                        if ($Session.Split(' ')[3] -eq 'Active') {
+                            [PSCustomObject]@{
+                                ComputerName = $Computer
+                                UserName     = $session.Split(' ')[0]
+                                SessionName  = $session.Split(' ')[1]
+                                SessionID    = $Session.Split(' ')[2]
+                                SessionState = $Session.Split(' ')[3]
+                                IdleTime     = $Session.Split(' ')[4]
+                                LogonTime    = $session.Split(' ')[5, 6, 7] -as [string] -as [datetime]
+                            } 
+                        } else {
+                            [PSCustomObject]@{
+                                ComputerName = $Computer
+                                UserName     = $session.Split(' ')[0]
+                                SessionName  = $null
+                                SessionID    = $Session.Split(' ')[1]
+                                SessionState = 'Disconnected'
+                                IdleTime     = $Session.Split(' ')[3]
+                                LogonTime    = $session.Split(' ')[4, 5, 6] -as [string] -as [datetime]
+                            }
+                        }
+                    }
+ 
+                    if ($PSBoundParameters.ContainsKey('Username')) {
+                        $UserInfo | Where-Object {$_.UserName -eq $UserName}
+                      } else {
+                        $UserInfo | Sort-Object LogonTime
+                    }
+                }
+            } catch {
+                Write-Error $_.Exception.Message
+ 
+            }
+        }
+    }
+ 
+    END {}
+}
+Get-LoggedInUser
+
+# List Disconnected Sessions
+$ListSessionsDisconnected = $Global:UserInfo | Where {$_.SessionState -like 'Disconnected'} | Select-Object -Property Username,SessionID
+
+# List Active Sessions
+$ListSessionsActive = $Global:UserInfo | Where {$_.SessionState -like 'Active'} | Select-Object -Property Username,SessionID
+
+# List Active RDP Sessions
+$listSessionsActiveRDP = $Global:UserInfo | Where {($_.SessionState -like 'Active') -and ($_.SessionName -like '*rdp*') } | Select-Object -Property Username,SessionID,SessionName
+
+# Add Logoff Arguement for server and supplied servername. 
+$Contcat = "/server:" + $ComputerSelect.Name 
+
+# Function to supply menu and check if session is avaiable and log off users
+function Show-LogOffOptions { try
+    {
+    $LogOffOptions = @"
+    Logoff User/s Options:
+    [1] Logoff Disconnected User/s
+    [2] Logoff RDP Active User/s
+    [3] Logoff All Active User/s
+    [4] Logoff All User/s
+    [5] Quit
+
+    Type a number and press enter 
+"@
+
+    $AskLofOffOptions = Read-Host -Prompt $LogOffOptions
+		    switch ($AskLofOffOptions) {
+		        "1" { if ($ListSessionsDisconnected.SessionID -eq $Null) {Write-host "No User/s Found `n" -ForegroundColor Red -NoNewline;Show-LogOffOptions}
+                        else{ 
+                            logoff $ListSessionsDisconnected.SessionID $Contcat }}
+                "2" { if ($listSessionsActiveRDP.SessionID -eq $Null) {Write-host "No User/s Found `n" -ForegroundColor Red -NoNewline;Show-LogOffOptions}
+                        else{ 
+                            logoff $listSessionsActiveRDP.SessionID $Contcat }}
+		    	"3" { if ($ListSessionsActive.SessionID -eq $Null) {Write-host "No User/s Found `n" -ForegroundColor Red -NoNewline;Show-LogOffOptions}
+                        else{
+                        logoff $ListSessionsActive.SessionID $Contcat }}
+                "4" { if (($ListSessionsActive.SessionID -eq $Null) -and ($ListSessionsDisconnected.SessionID -eq $Null)) {Write-host "No User/s Found `n" -ForegroundColor Red -NoNewline;Show-LogOffOptions}
+                        else{
+                        logoff $ListSessionsDisconnected.SessionID $Contcat ; logoff $ListSessionsActive.SessionID $Contcat }}
+                "5" { break }
+
+		    	Default {Write-Host "One of the listed numbers was not entered";Show-LogOffOptions}
+		    }
+        }
+ catch {
+        Write-host "No User/s Found" -ForegroundColor Red -NoNewline
+        
+    }
+}
+Show-LogOffOptions
+
+# Clears the Variables, this stops any issue with the variables bring back any previous held information
+Remove-Variable * -ErrorAction SilentlyContinue
---
+++ Powershell/AD Email Only Users and Sage200 Web Only Users MemberOf Report.ps1
@@ -0,0 +1,173 @@
+﻿# HTML Code
+
+$header = "
+
+<style>
+
+    body {
+        padding: 0em 1em;
+        }
+
+    h1 {
+        font-family: Arial, Helvetica, sans-serif;
+        color: #e68a00;
+        font-size: 28px;
+       } 
+
+
+    h2 {
+        font-family: Arial, Helvetica, sans-serif;
+        color: #000099;
+        font-size: 16px;
+        display:inline;
+        }
+
+    h3 {
+        font-family: Arial, Helvetica, sans-serif;
+        color: #FFFFFF;
+        font-size: 24px;
+        display:inline;
+        }
+    
+    h4 {
+        font-family: Arial, Helvetica, sans-serif;
+        color: #008000;
+        font-size: 16px;
+        display:inline;
+        }
+
+    h5 {
+        font-family: Arial, Helvetica, sans-serif;
+        color: #e68a00;
+        font-size: 16px;
+        display:inline;
+        }
+
+    p.a {
+        font-style: italic;
+        font-size: 12px;
+        }
+
+    pd {
+        font-family: Arial, Helvetica, sans-serif;
+        color: #008000;
+        font-size: 16px;
+        display:inline;
+        padding: 5px
+        }
+
+   table {
+		font-size: 12px;
+		border: 0px; 
+		font-family: Arial, Helvetica, sans-serif;
+	    } 
+	
+    td {
+		padding: 4px;
+		margin: 0px;
+		border: 0;
+	    }
+	
+    th {
+        background: #395870;
+        background: linear-gradient(#49708f, #293f50);
+        color: #fff;
+        font-size: 11px;
+        text-transform: uppercase;
+        padding: 10px 15px;
+        vertical-align: middle;
+	    }
+
+    tbody tr:nth-child(even) {
+        background: #f0f0f2;
+        }
+
+    #CreationDate {
+        font-family: Arial, Helvetica, sans-serif;
+        color: #ff3300;
+        font-size: 12px;
+        }
+
+    button {
+        line-height: 1.5em;
+        height: 1.5em;
+        width: 1.5em;
+        border-radius: 50%;
+        display: inline;
+        position: inline;
+        }
+
+    /* Style the button that is used to open and close the collapsible content */
+
+    .collapsible {
+        background-color: #008000;
+        color: #444;
+        padding: 0px 0px;
+        border: none;
+        outline: none;
+        font-size: 15px;
+        }
+
+    /* Add a background color to the button if it is clicked on (add the .active class with JS), and when you move the mouse over it (hover) */
+
+    .active, .collapsible:hover {
+        background-color: #66CDAA;
+        }
+
+    /* Style the collapsible content. Note: hidden by default */
+
+    .content {
+        padding: 0 18px;
+        display: none;
+        overflow: hidden;
+        }
+
+</style>
+
+”
+
+# Page Title
+
+$PageTitle = "<h1>Email Only and Sage200 Web Only Users MemberOf Report</h1>"
+
+# Gets a list of OU
+$OUList = Get-ADOrganizationalUnit -Filter {(Name -eq 'Email Only Users') -or (Name -eq 'Sage200 Web Only Users')} -Properties Name,DistinguishedName | Sort-Object | Select-Object -Property Name,DistinguishedName
+
+# Gets the AD Users as per the OU selected, Added the LastLogOnDate properties, display the Name. Enablesand LastLogOnDate
+$ADUsers = foreach ($OUName in $OUList){
+Get-ADUser -SearchBase $OUName.DistinguishedName -Filter * -Properties LastLogOnDate,Company,samAccountName,memberof,ExtensionAttribute2 | Where {$_.Enabled -eq 1}  | Select-Object -Property Company,Name,ExtensionAttribute2,LastLogOnDate,@{name="MemberOf";expression={$_.MemberOf -join "; "}}
+}
+
+# Display the OU Path
+#$OU.DistinguishedName
+#Write-Host "------------"
+#$ADUsers | Sort-Object -Property Company, Name
+$ADUsersHTML = $ADUsers | ConvertTo-Html
+
+
+# Code to create HTML Report
+
+$Report = ConvertTo-Html -Body "$PageTitle
+ $ADUsersHTML <p></p>
+  " -Title "Company Status Report" -Head $header -PostContent "<script>
+  var coll = document.getElementsByClassName('collapsible');
+        var i;
+        
+        for (i = 0; i < coll.length; i++) {
+          coll[i].addEventListener('click', function() {
+            this.classList.toggle('active');
+            var content = this.nextElementSibling;
+            if (content.style.display === 'block') {
+              content.style.display = 'none';
+            } else {
+              content.style.display = 'block';
+            }
+          });
+        }
+        </script><p id='CreationDate'>Creation Date: $(Get-Date)</p>"
+$Report | Out-File -FilePath c:\Temp\AD-Email-Only-Users-and-Sage200-Web-Only-Users-MemberOf-Report.html
+Invoke-Expression c:\Temp\AD-Email-Only-Users-and-Sage200-Web-Only-Users-MemberOf-Report.html
+
+
+# Clears the Variables, this stops any issue with the variables bring back any previous held information
+Remove-Variable * -ErrorAction SilentlyContinue
