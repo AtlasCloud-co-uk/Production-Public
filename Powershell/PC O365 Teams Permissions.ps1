91956d160d37839a972c907bca77ba5ff4c37d3f
Darren Edgson <darren.edgson@atlascloud.co.uk>
2023-03-13 10:09:53 +0000

    Update PC O365 Teams Permissions.ps1
    Updated

--- Powershell/PC O365 Teams Permissions.ps1
+++ Powershell/PC O365 Teams Permissions.ps1
@@ -1,4 +1,4 @@
-#############################################################################
+﻿#############################################################################
 #If Powershell is running the 32-bit version on a 64-bit machine, we 
 #need to force powershell to run in 64-bit mode .
 #############################################################################
@@ -19,6 +19,14 @@ write-host "Starting Main script body"
 #End
 #############################################################################  
 
+# Create a table
+
+$tbl = New-Object System.Data.DataTable "Data"
+$col1 = New-Object System.Data.DataColumn TeamsName
+$col2 = New-Object System.Data.DataColumn UserName
+$tbl.Columns.Add($col1)
+$tbl.Columns.Add($col2)
+
 # Connect to AD Service
 Connect-MsolService
 
@@ -29,16 +37,26 @@ $TenantsListSelect = Get-MsolPartnerContract | Sort-object Name | Select-Object 
 Connect-ExchangeOnline -DelegatedOrganization $TenantsListSelect.DefaultDomainName -ShowBanner:$false
 
 # Get Unified Groups and Users
-$TGroups = Get-UnifiedGroup
-foreach ($TGroup in $TGroups)
+$TGroups = Get-UnifiedGroup | Sort-Object DisplayName
+
+$Output = foreach ($TGroup in $TGroups)
 {
-Write-Host ""
-$TGroup.DisplayName
-Get-UnifiedGroupLinks -Identity $TGroup.DisplayName -LinkType Member | Select-Object Name | ft -AutoSize
+    $TeamMembers = Get-UnifiedGroupLinks -Identity $TGroup.DisplayName -LinkType Member | Sort-Object Name | Select-Object Name
+    foreach ($GetUser in $TeamMembers) {
+
+        $row = $tbl.NewRow()
+            $row.TeamsName = $TGroup.DisplayName
+            $row.UserName = $GetUser.Name
+        $tbl.Rows.Add($row)                  
+    }  
+
+
 }
+
+$tbl | Out-GridView -Title "Team Names and Members"
 
 # Close Connection
 Get-PSSession | Remove-PSSession
 
 # Clears the Variables, this stops any issue with the variables bring back any previous held information
-Remove-Variable * -ErrorAction SilentlyContinue
+#Remove-Variable * -ErrorAction SilentlyContinue
