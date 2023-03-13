d87008bd9918f2b78f09dab94d555020e08a8502
martinpurvis-atlas <martinpurvis2@gmail.com>
2023-03-13 14:35:20 +0000

    Added 64 bit powershell to be forced

--- Powershell/VMW List VMs backed up using a tag.ps1
+++ Powershell/VMW List VMs backed up using a tag.ps1
@@ -5,6 +5,20 @@
     2. Specifying where to export a csv if they so choose
 #>
 
+#############################################################################
+#If Powershell is running the 32-bit version on a 64-bit machine, we 
+#need to force powershell to run in 64-bit mode .
+#############################################################################
+if ($env:PROCESSOR_ARCHITEW6432 -eq "AMD64") {
+    write-warning "Opening 64-bit powershell....."
+    if ($myInvocation.Line) {
+        &"$env:WINDIR\sysnative\windowspowershell\v1.0\powershell.exe" -NoProfile $myInvocation.Line
+    }else{
+        &"$env:WINDIR\sysnative\windowspowershell\v1.0\powershell.exe" -NoProfile -file "$($myInvocation.InvocationName)" $args
+    }
+exit $lastexitcode
+}
+
 Import-Module VMware.VimAutomation.Core
 
 $VSphereServerImport = Import-Csv -Path "C:\Powershell Scripts\CSV\VMServers.csv"
