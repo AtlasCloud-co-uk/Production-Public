3b647a7f610ec6791e763e24d3554cd766959e66
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2023-04-19 12:13:37 +0100

    Update O365 AD Group Members.ps1
    Removed DOT from Script caused by upload.

--- Powershell Link Scripts/O365/O365 AD Group Members.ps1
+++ Powershell Link Scripts/O365/O365 AD Group Members.ps1
@@ -1,3 +1,3 @@
-﻿[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
+[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
 $ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/AtlasCloud-co-uk/Production-Public/main/Powershell/O365%20AD%20Group%20Members.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)
