7204de5a0996a192045af51a143f2b9a4fd3f84e
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2023-04-19 12:16:07 +0100

    Update PC O365 User Licence Report.ps1
    Removed DOT from Script caused by upload.

--- Powershell Link Scripts/Partner Center/PC O365 User Licence Report.ps1
+++ Powershell Link Scripts/Partner Center/PC O365 User Licence Report.ps1
@@ -1,3 +1,3 @@
-﻿[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
+[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
 $ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/AtlasCloud-co-uk/Production-Public/main/Powershell/PC%20O365%20User%20Licence%20Report.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)
