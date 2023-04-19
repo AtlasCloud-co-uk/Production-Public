054ab2662f0b2fd5fa2d1e4186a47b5063a537a5
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2023-04-19 12:03:31 +0100

    Update AD Enabled Account Missing Company Name or ExtensionAttribute2.ps1
    Removed DOT from Script caused by upload.

--- Powershell Link Scripts/Active Directory/AD Enabled Account Missing Company Name or ExtensionAttribute2.ps1
+++ Powershell Link Scripts/Active Directory/AD Enabled Account Missing Company Name or ExtensionAttribute2.ps1
@@ -1,3 +1,3 @@
-﻿[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
+[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
 $ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/AtlasCloud-co-uk/Production-Public/main/Powershell/AD%20Enabled%20Account%20Missing%20Company%20Name%20or%20ExtensionAttribute2.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)
