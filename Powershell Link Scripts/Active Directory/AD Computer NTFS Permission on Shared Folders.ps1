0ca9d8670b0843bfbea724dc2d18994c4a9f5dd0
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2023-04-19 11:54:51 +0100

    Update AD Computer NTFS Permission on Shared Folders.ps1
    Removed DOT from Script caused by upload.

--- Powershell Link Scripts/Active Directory/AD Computer NTFS Permission on Shared Folders.ps1
+++ Powershell Link Scripts/Active Directory/AD Computer NTFS Permission on Shared Folders.ps1
@@ -1,3 +1,3 @@
-﻿[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
+[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
 $ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/AtlasCloud-co-uk/Production-Public/main/Powershell/AD%20Computer%20NTFS%20Permission%20on%20Shared%20Folders.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)
