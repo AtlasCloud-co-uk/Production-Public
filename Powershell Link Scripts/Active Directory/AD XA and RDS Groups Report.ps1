5087181e4d6362695ca0c3f91f8d0aca6d13e6ee
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2023-04-19 12:11:17 +0100

    Update AD XA and RDS Groups Report.ps1
    Removed DOT from Script caused by upload.

--- Powershell Link Scripts/Active Directory/AD XA and RDS Groups Report.ps1
+++ Powershell Link Scripts/Active Directory/AD XA and RDS Groups Report.ps1
@@ -1,3 +1,3 @@
-﻿[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
+[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
 $ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/AtlasCloud-co-uk/Production-Public/main/Powershell/AD%20XA%20and%20RDS%20Groups%20Report.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)
