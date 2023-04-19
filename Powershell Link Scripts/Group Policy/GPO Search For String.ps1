16bea247aa74d006be180e8e161c1b3342d6bcad
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2023-04-19 12:12:20 +0100

    Update GPO Search For String.ps1
    Removed DOT from Script caused by upload.

--- Powershell Link Scripts/Group Policy/GPO Search For String.ps1
+++ Powershell Link Scripts/Group Policy/GPO Search For String.ps1
@@ -1,3 +1,3 @@
-﻿[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
+[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
 $ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/AtlasCloud-co-uk/Production-Public/main/Powershell/GPO%20Search%20For%20String.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)
