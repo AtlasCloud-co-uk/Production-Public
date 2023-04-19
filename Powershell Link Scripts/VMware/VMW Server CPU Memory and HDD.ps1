8557666c9c239423e89e6093f747bbfd318b1244
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2023-04-19 12:18:28 +0100

    Update VMW Server CPU Memory and HDD.ps1
    Removed DOT from Script caused by upload.

--- Powershell Link Scripts/VMware/VMW Server CPU Memory and HDD.ps1
+++ Powershell Link Scripts/VMware/VMW Server CPU Memory and HDD.ps1
@@ -1,3 +1,3 @@
-﻿[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
+[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
 $ScriptFromGitHub = Invoke-WebRequest -UseBasicParsing -Uri https://raw.githubusercontent.com/AtlasCloud-co-uk/Production-Public/main/Powershell/VMW%20Server%20CPU%20Memory%20and%20HDD.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)
