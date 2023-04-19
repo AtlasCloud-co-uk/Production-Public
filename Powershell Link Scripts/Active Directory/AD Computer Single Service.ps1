90b648ac7e9ac50f4d0e8324e3b35ab8545546dc
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2023-04-19 11:55:19 +0100

    Update AD Computer Single Service.ps1
    Removed DOT from Script caused by upload.

--- Powershell Link Scripts/Active Directory/AD Computer Single Service.ps1
+++ Powershell Link Scripts/Active Directory/AD Computer Single Service.ps1
@@ -1,3 +1,3 @@
-﻿[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
+[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
 $ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/AtlasCloud-co-uk/Production-Public/main/Powershell/AD%20Computer%20Single%20Service.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)
