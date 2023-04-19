d563e4533ad2f70530034c8c5a4c63f8dbfbaf6f
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2023-04-19 11:55:04 +0100

    Update AD Computer Shared Folder Permissions.ps1
    Removed DOT from Script caused by upload.

--- Powershell Link Scripts/Active Directory/AD Computer Shared Folder Permissions.ps1
+++ Powershell Link Scripts/Active Directory/AD Computer Shared Folder Permissions.ps1
@@ -1,3 +1,3 @@
-﻿[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
+[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
 $ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/AtlasCloud-co-uk/Production-Public/main/Powershell/AD%20Computer%20Shared%20Folder%20Permissions.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)
