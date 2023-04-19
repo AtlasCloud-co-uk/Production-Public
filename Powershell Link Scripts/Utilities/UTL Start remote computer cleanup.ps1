a550741566ef8bfec56eb69d4b7d0f0b8362ceed
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2023-04-19 12:17:50 +0100

    Update UTL Start remote computer cleanup.ps1
    Removed DOT from Script caused by upload.

--- Powershell Link Scripts/Utilities/UTL Start remote computer cleanup.ps1
+++ Powershell Link Scripts/Utilities/UTL Start remote computer cleanup.ps1
@@ -1,3 +1,3 @@
-﻿[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
+[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
 $ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/AtlasCloud-co-uk/Production-Public/main/Powershell/UTL%20Start%20remote%20computer%20cleanup.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)
