e45ded0aa987f023d9c270f7e72d4c6318f6d5fe
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2023-04-19 12:07:01 +0100

    Update AD Multiple Computer Uptime.ps1
    Removed DOT from Script caused by upload.

--- Powershell Link Scripts/Active Directory/AD Multiple Computer Uptime.ps1
+++ Powershell Link Scripts/Active Directory/AD Multiple Computer Uptime.ps1
@@ -1,3 +1,3 @@
-﻿[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
+[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
 $ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/AtlasCloud-co-uk/Production-Public/main/Powershell/AD%20Multiple%20Computer%20Uptime.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)
