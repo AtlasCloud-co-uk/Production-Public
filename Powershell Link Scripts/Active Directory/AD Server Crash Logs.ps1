8d0511ddacf3209f360a5ec9fb3a66fdf8cfcb5e
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2023-04-19 12:10:33 +0100

    Update AD Server Crash Logs.ps1
    Removed DOT from Script caused by upload.

--- Powershell Link Scripts/Active Directory/AD Server Crash Logs.ps1
+++ Powershell Link Scripts/Active Directory/AD Server Crash Logs.ps1
@@ -1,3 +1,3 @@
-﻿[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
+[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
 $ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/AtlasCloud-co-uk/Production-Public/main/Powershell/AD%20Server%20Crash%20Logs.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)
