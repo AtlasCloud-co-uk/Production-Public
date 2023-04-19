2a3b3fef9f36e1cff3778e4e343d2be62f21ad15
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2023-04-19 11:49:51 +0100

    Update AD Company Status Report.ps1
    Removed DOT from Script caused by upload.

--- Powershell Link Scripts/Active Directory/AD Company Status Report.ps1
+++ Powershell Link Scripts/Active Directory/AD Company Status Report.ps1
@@ -1,3 +1,3 @@
-﻿[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
+[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
 $ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/AtlasCloud-co-uk/Production-Public/main/Powershell/AD%20Company%20Status%20Report.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)
