8a81a13725f2e2e11746c76f987f405564395d36
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2023-04-19 12:13:50 +0100

    Update O365 Change Password on Next Logon with Select Users.ps1
    Removed DOT from Script caused by upload.

--- Powershell Link Scripts/O365/O365 Change Password on Next Logon with Select Users.ps1
+++ Powershell Link Scripts/O365/O365 Change Password on Next Logon with Select Users.ps1
@@ -1,3 +1,3 @@
-﻿[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
+[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
 $ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/AtlasCloud-co-uk/Production-Public/main/Powershell/O365%20Change%20Password%20on%20Next%20Logon%20with%20Select%20Users
 Invoke-Expression $($ScriptFromGitHub.Content)
