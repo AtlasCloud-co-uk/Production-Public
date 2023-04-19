4a3aab509ebe8f632e3bef4501758de4faaa0383
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2023-04-19 12:09:17 +0100

    Update AD OU Set All To Change Password On Next Logon.ps1
    Removed DOT from Script caused by upload.

--- Powershell Link Scripts/Active Directory/AD OU Set All To Change Password On Next Logon.ps1
+++ Powershell Link Scripts/Active Directory/AD OU Set All To Change Password On Next Logon.ps1
@@ -1,3 +1,3 @@
-﻿[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
+[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
 $ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/AtlasCloud-co-uk/Production-Public/main/Powershell/AD%20OU%20Set%20All%20To%20Change%20Password%20On%20Next%20Logon.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)
