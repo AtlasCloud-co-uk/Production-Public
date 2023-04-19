4a8e54fb429d29e55524f19137103f308886af5d
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2023-04-19 12:18:17 +0100

    Update VMW List VMs backed up using a tag.ps1
    Removed DOT from Script caused by upload.

--- Powershell Link Scripts/VMware/VMW List VMs backed up using a tag.ps1
+++ Powershell Link Scripts/VMware/VMW List VMs backed up using a tag.ps1
@@ -1,3 +1,3 @@
-﻿[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
+[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
 $ScriptFromGitHub = Invoke-WebRequest -UseBasicParsing -Uri https://raw.githubusercontent.com/AtlasCloud-co-uk/Production-Public/main/Powershell/VMW%20List%20VMs%20backed%20up%20using%20a%20tag.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)
