b9924a5f597d5f0407a3b50bd3370632b635685a
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2023-04-19 12:10:47 +0100

    Update AD Server OS list.ps1
    Removed DOT from Script caused by upload.

--- Powershell Link Scripts/Active Directory/AD Server OS list.ps1
+++ Powershell Link Scripts/Active Directory/AD Server OS list.ps1
@@ -1,3 +1,3 @@
-﻿[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
+[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
 $ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/AtlasCloud-co-uk/Production-Public/main/Powershell/AD%20Server%20OS%20list.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)
