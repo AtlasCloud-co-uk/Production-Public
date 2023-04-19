c36a05c540220208e5ae3cff8ec87a7271de2a9b
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2023-04-19 12:09:01 +0100

    Update AD OU Set All Passwords To Expire.ps1
    Removed DOT from Script caused by upload.

--- Powershell Link Scripts/Active Directory/AD OU Set All Passwords To Expire.ps1
+++ Powershell Link Scripts/Active Directory/AD OU Set All Passwords To Expire.ps1
@@ -1,3 +1,3 @@
-﻿[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
+[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
 $ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/AtlasCloud-co-uk/Production-Public/main/Powershell/AD%20OU%20Set%20All%20Passwords%20To%20Expire.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)
