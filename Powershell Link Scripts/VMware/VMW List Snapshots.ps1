bbf0bdcc2ebd6aadaadeca4ce563c399f997ceed
DarrenEdgson <97220984+DarrenEdgson@users.noreply.github.com>
2023-04-19 12:18:04 +0100

    Update VMW List Snapshots.ps1
    Removed DOT from Script caused by upload.

--- Powershell Link Scripts/VMware/VMW List Snapshots.ps1
+++ Powershell Link Scripts/VMware/VMW List Snapshots.ps1
@@ -1,3 +1,3 @@
-﻿[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
+[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
 $ScriptFromGitHub = Invoke-WebRequest -UseBasicParsing -Uri https://raw.githubusercontent.com/AtlasCloud-co-uk/Production-Public/main/Powershell/VMW%20List%20Snapshots.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)
