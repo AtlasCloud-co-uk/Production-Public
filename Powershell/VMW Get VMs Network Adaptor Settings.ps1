72431c1168b110c793cbeddd2d0913ff308c762e
Darren Edgson <darren.edgson@atlascloud.co.uk>
2023-05-15 08:54:00 +0100

    Create VMW Get VMs Network Adaptor Settings.ps1

---
+++ Powershell/VMW Get VMs Network Adaptor Settings.ps1
@@ -0,0 +1,21 @@
+﻿# Imports the Server names
+$VSphereServerImport = Import-Csv -Path "C:\Powershell Scripts\CSV\VMServers.csv"
+
+# Connect to the VMWare server and pulls back the snapsots into a grid-view
+Connect-VIServer $VSphereServerImport.Server
+
+# Get VM's Names
+$VM = Get-Cluster -Name production | Get-VM
+
+# Get Network Adaptor Info
+foreach ($VMS in $VM) {
+
+$Nettype = Get-NetworkAdapter -VM $VMS
+
+foreach ($nic in $Nettype) {
+
+"{0} {1} {2}" -f $VMS.Name, $nic.Name, $nic.Type
+
+}
+
+} 
