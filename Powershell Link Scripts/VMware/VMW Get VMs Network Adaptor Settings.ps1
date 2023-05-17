7e9ec1c05ed99310fd3a5726e2010dfc27d02774
Darren Edgson <darren.edgson@atlascloud.co.uk>
2023-05-17 09:24:53 +0100

    New

---
+++ Powershell Link Scripts/VMware/VMW Get VMs Network Adaptor Settings.ps1
@@ -0,0 +1,3 @@
+﻿[System.Net.WebRequest]::DefaultWebProxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials
+$ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/AtlasCloud-co-uk/Production-Public/main/Powershell/VMW%20Get%20VMs%20Network%20Adaptor%20Settings.ps1
+Invoke-Expression $($ScriptFromGitHub.Content)
---
+++ Powershell/AD Email Company Account Type Report.ps1
@@ -0,0 +1,33 @@
+﻿# Email Parameters Import
+$EmailImport = Import-Csv -Path "C:\Powershell Scripts\CSV\EmailParameters.csv"
+
+# Email Parameters
+
+$Subject = $EmailImport.Subject 
+$body = $EmailImport.Body
+$emailHost = $EmailImport.Host
+$emailFrom = $EmailImport.From
+$emailsTo=$EmailImport.To
+$emailbody=$body
+$attachment1 = $EmailImport.Attchment 
+
+#End of parameters
+
+# Email Script Build
+
+$msg = New-Object System.Net.Mail.MailMessage
+$msg.from = ($emailFrom)
+$msg.sender = ($emailFrom)
+$msg.to.add($emailsTo)
+$msg.Subject = $Subject
+$msg.Body = $emailbody
+$msg.isBodyhtml = $true   
+
+$att = new-object System.Net.Mail.Attachment($attachment1)
+$msg.Attachments.add($att)
+$smtp = New-Object System.Net.Mail.SmtpClient $emailHost
+$smtp.send($msg)
+$att.Dispose()
+
+# Clears the Variables, this stops any issue with the variables bring back any previous held information
+Remove-Variable * -ErrorAction SilentlyContinue
